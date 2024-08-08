81f57e10ff5c3f1707cb5d1f6f0b60806a6460fb
Daniel Sarmiento <daniel.sarmiento@dafiti.com.co>
2024-08-07 23:30:09 -0500

    commit

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const initializeBrowser = async (proxy) => {
     const browser = await puppeteer.launch({
         headless: true, 
-        args: ['--no-sandbox', '--disable-setuid-sandbox',`--proxy-server=${proxy}`],
+        args: ['--no-sandbox',`--proxy-server=${proxy}`],
         ignoreHTTPSErrors: true,
         dumpio: false
       });
